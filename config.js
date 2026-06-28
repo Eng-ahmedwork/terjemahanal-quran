bc36129c6d96b0ec9a2d1f54815b982eabfadd67
Eng-ahmedwork <ahmedwork549@gmail.com>
2026-06-28 23:50:36 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// --- Global Configuration ---
+// This file stores API keys and Firebase configurations.
+// Make sure to load this file before app.js in your index.html.
+
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA5GlkVhVSGPBGyhEEQj97BXhF5ubPxs5c",
+    authDomain: "translation-quran-b813b.firebaseapp.com",
+    databaseURL: "https://translation-quran-b813b-default-rtdb.firebaseio.com",
+    projectId: "translation-quran-b813b",
+    storageBucket: "translation-quran-b813b.firebasestorage.app",
+    messagingSenderId: "599555250100",
+    appId: "1:599555250100:web:05c94da4da3d9d3a4944e1",
+    measurementId: "G-ZQ25NZGZEL"
+};
+///AI Model
+const GROQ_API_KEY = "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";
+const OPENROUTER_API_KEY = "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";
